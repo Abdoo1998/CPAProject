b97117aac5e29535f477179ba9fd5022f49d8e0c
Pablo Gorostiaga <pg1816@ic.ac.uk>
2017-07-29 23:05:03 +0200

    change

--- src/GUI/CPANewTaskGUI.java
+++ src/GUI/CPANewTaskGUI.java
@@ -36,8 +36,8 @@ public class CPANewTaskGUI extends JFrame implements ActionListener {
 
     /*Default fields*/
     private static final int DEFAULT_COLUMNSIZE = 20;
-    private static final int DEFAULT_WIDTH = 450;
-    private static final int DEFAULT_HEIGHT = 200;
+    private static final int DEFAULT_WIDTH = 650;
+    private static final int DEFAULT_HEIGHT = 400;
     private static final Color DEFAULT_COLOR = new Color(20, 140, 5);
 
       /*
