70eb32b3e06efcc3c38c241c531dcab38323dccf
Pablo Gorostiaga <pg1816@ic.ac.uk>
2017-08-07 00:40:27 +0200

    added java doc for overalltaskViewComponent

--- src/GUI/OverallTaskViewComponent.java
+++ src/GUI/OverallTaskViewComponent.java
@@ -6,12 +6,28 @@ import application.OverallTask;
 import javax.swing.*;
 import java.awt.*;
 
+/**
+ * Represents the OverallTask GUI component in the Task View tab of the main application frame.
+ *
+ * @author gorosgobe
+ */
 public class OverallTaskViewComponent extends JPanel {
 
+    /** Label representing the name of the OverallTask*/
     private final JLabel name;
+    /** Label representing the duration of the OverallTask*/
     private final JLabel duration;
+    /** Label representing the starting time of the OverallTask*/
     private final JLabel startTime;
 
+    /**
+     * Constructor for the OverallTaskViewComponent that takes the task that will populate it and the dimension
+     * of the component. The dimension is required so the CPAProjectApplicationGUI can specify who to modify its
+     * task panel within the scroll pane to add more OverallTaskViewComponents. This constructor also initalises
+     * the task's layout and positions its components, and then sets the preferred size to the dimension given.
+     * @param task the overall task to represent with the component
+     * @param dimension the dimension of the component
+     */
     public OverallTaskViewComponent(OverallTask task, Dimension dimension) {
         this.name = new JLabel(task.getTaskName());
         name.setFont(FontCollection.BIG_BOLD_FONT);
@@ -25,21 +41,28 @@ public class OverallTaskViewComponent extends JPanel {
         setPreferredSize(dimension);
     }
 
+    /**
+     * Positions the name, duration and starting time in the component with a GridBagLayout. For more information
+     * on GridBagLayout and GridBagConstraints, see the Java tutorials.
+     */
     private void setTaskViewComponentLayout() {
         setLayout(new GridBagLayout());
 
+        //constraints for the name
         GridBagConstraints nameConstraints = new GridBagConstraints();
         nameConstraints.gridx = 0;
         nameConstraints.gridy = 0;
         nameConstraints.insets = new Insets(0, 10, 10, 10);
         add(name, nameConstraints);
 
+        //constraints for the duration
         GridBagConstraints durationConstraints = new GridBagConstraints();
         durationConstraints.gridx = 0;
         durationConstraints.gridy = 1;
         durationConstraints.insets = new Insets(0, 10, 10, 10);
         add(duration, durationConstraints);
 
+        //constraints for the starting time
         GridBagConstraints startTimeConstraints = new GridBagConstraints();
         startTimeConstraints.gridx = 0;
         startTimeConstraints.gridy = 2;
