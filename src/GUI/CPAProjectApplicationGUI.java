302c9bb778505abcb7904bbca0ae79309c17a0cf
Pablo Gorostiaga <pg1816@ic.ac.uk>
2017-08-07 01:12:58 +0200

    added javadoc for CPAProjectApplicationGUI

--- src/GUI/CPAProjectApplicationGUI.java
+++ src/GUI/CPAProjectApplicationGUI.java
@@ -21,7 +21,6 @@ public class CPAProjectApplicationGUI extends JFrame {
     private JPanel optimalPlanView;
     /*Holds all Overall Tasks to render*/
     private List<OverallTask> tasks;
-    //private List<OverallTaskViewComponent> overallTaskViewComponentList;
 
     /* Name of the application, shown at the top of the frame */
     private static final String APPLICATION_NAME = "CPAProject";
@@ -42,6 +41,9 @@ public class CPAProjectApplicationGUI extends JFrame {
     private static final int SCROLL_BAR_SPEED = 18;
 
 
+    /**
+     * Constructs the application's main GUI. Initialises and sets the menu bar and the tabbed pane.
+     */
     public CPAProjectApplicationGUI() {
         this.tasks = new LinkedList<>();
 
@@ -56,15 +58,25 @@ public class CPAProjectApplicationGUI extends JFrame {
         initialiseAndSetTabbedPane();
     }
 
+    /**
+     * Gets the list containing all tasks held by the GUI.
+     * @return the list containing all tasks in the GUI.
+     */
     public List<OverallTask> getTasks() {
         return tasks;
     }
 
+    /**
+     * Adds a task to the task view and to the task list. Recalculates size of scrollable pane with addition
+     * of elements.
+     * @param task the task to add
+     */
     public void addOverallTask(OverallTask task) {
         tasks.add(task);
         Dimension componentDimension = new Dimension(OVERALL_TASK_VIEW_COMPONENT_WIDTH, OVERALL_TASK_VIEW_COMPONENT_HEIGHT);
         OverallTaskViewComponent overallTaskViewComponent = new OverallTaskViewComponent(task, componentDimension);
 
+        //constraints for the nth component, with row and column limits specified by constants
         GridBagConstraints constraints = new GridBagConstraints();
         constraints.gridx = (tasks.size() - 1) % MAX_TASK_VIEW_NUM_WIDTH;
         constraints.gridy = (tasks.size() - 1) / MAX_TASK_VIEW_NUM_WIDTH;
@@ -75,16 +87,31 @@ public class CPAProjectApplicationGUI extends JFrame {
         constraints.anchor = GridBagConstraints.BASELINE;
         taskPanel.add(overallTaskViewComponent, constraints);
 
+        //recalculates size of task panel in scrollable pane
         if (tasks.size() % MAX_TASK_VIEW_NUM_WIDTH == 0 && tasks.size() > BOUNDARY_SCROLLABLE_ROWS * MAX_TASK_VIEW_NUM_WIDTH) {
             taskPanel.setSize(new Dimension((int) taskPanel.getPreferredSize().getWidth(),
                     (int) taskPanel.getPreferredSize().getHeight() + OVERALL_TASK_VIEW_COMPONENT_HEIGHT));
             taskPanel.revalidate();
         }
-
-
     }
 
+    /**
+     * Adds all tasks to the Task View. Useful for saved data on file, we parse the file data and construct
+     * a list, and then show it on the GUI by calling this method.
+     * @param taskList the list of tasks to be shown/added on the GUI
+     */
+    public void addAllOverallTasks(List<OverallTask> taskList) {
+        for (OverallTask t : taskList) {
+            addOverallTask(t);
+        }
+    }
+
+    /**
+     * Removes the given task from the task view and redraws the tasks.
+     * @param task the task to be removed
+     */
     public void removeOverallTask(OverallTask task) {
+        //Note: recalculating the position of every task might be more expensive than drawing all tasks again, TBD.
         //TODO: PABLO
         tasks.remove(task);
     }
@@ -106,16 +133,25 @@ public class CPAProjectApplicationGUI extends JFrame {
         setVisible(true);
     }
 
+    /**
+     * Initialises and sets the tabbed pane in the main frame.
+     */
     private void initialiseAndSetTabbedPane() {
         this.tabbedPane = new JTabbedPane();
+        //set each tab's inside contents
         setTaskView();
         setOptimalPlanView();
         tabbedPane.setFont(FontCollection.DEFAULT_FONT_PLAIN);
+        //add tabs
         tabbedPane.addTab(TASK_VIEW_TAB_STRING, taskView);
         tabbedPane.addTab(OPTIMAL_VIEW_TAB_STRING, optimalPlanView);
+        //add tabbed pane itself to main frame
         add(tabbedPane);
     }
 
+    /**
+     * Initialises and sets the menu bar.
+     */
     private void initialiseAndSetJMenuBar() {
         this.menuBar = new JMenuBar();
         menuBar.setPreferredSize(new Dimension(MENU_WIDTH, MENU_HEIGHT));
@@ -123,16 +159,20 @@ public class CPAProjectApplicationGUI extends JFrame {
         setJMenuBar(menuBar);
     }
 
-
+    /**
+     * Sets the task view, by creating a scrolling pane with its client being a JPanel.
+     */
     private void setTaskView() {
         //initially no tasks are provided to task panel, so layout calculations will be made in addTask
         this.taskPanel = new JPanel();
         //sets layout to gridbag layout
         taskPanel.setLayout(new GridBagLayout());
-
+        //initialises scrolling task view with client being task JPanel
         this.taskView = new JScrollPane(taskPanel);
+        //sets vertical scrolling
         taskView.createVerticalScrollBar();
         taskView.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
+        //blocks horizontal scrolling
         taskView.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);
         taskView.setPreferredSize(new Dimension(APPLICATION_WIDTH, APPLICATION_HEIGHT - MENU_HEIGHT));
         taskView.setFont(FontCollection.DEFAULT_FONT_PLAIN);
@@ -141,7 +181,11 @@ public class CPAProjectApplicationGUI extends JFrame {
 
     }
 
+    /**
+     * Sets the optimal plan view.
+     */
     private void setOptimalPlanView() {
+        //TODO: Pablo, complete.
         this.optimalPlanView = new JPanel();
         optimalPlanView.setPreferredSize(new Dimension(APPLICATION_WIDTH, APPLICATION_HEIGHT - MENU_HEIGHT));
         optimalPlanView.setFont(FontCollection.DEFAULT_FONT_PLAIN);
